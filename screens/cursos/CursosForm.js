e71965a5922f84cba0214fbf9e46320860cb2926
Orion Teles de Mesquita <orionteles@gmail.com>
2023-10-09 21:18:11 -0300

    Formulário de Curso

--- screens/cursos/CursosForm.js
+++ screens/cursos/CursosForm.js
@@ -1,11 +1,51 @@
-import React from 'react'
-import { Text } from 'react-native-paper'
+import React, { useState } from 'react'
+import { ScrollView } from 'react-native'
+import { Button, Text, TextInput } from 'react-native-paper'
 
 const CursosForm = () => {
+
+  const [dados, setDados] = useState({})
+
+  function handleChange(valor, campo) {
+    setDados({...dados, [campo]: valor })
+  }
+
+  function salvar() {
+    console.log(dados)
+  }
+
   return (
-    <>
-        <Text>Formulário de Curso</Text>
-    </>
+    <ScrollView style={{ margin: 15 }}>
+      <Text>Formulário de Curso</Text>
+
+      <TextInput
+        style={{ marginTop: 10 }}
+        mode='outlined'
+        label='Nome'
+        value={dados.nome}
+        onChangeText={(valor) => handleChange(valor, 'nome')}
+      />
+
+      <TextInput
+        style={{ marginTop: 10 }}
+        mode='outlined'
+        label='Duração'
+        keyboardType='decimal-pad'
+        value={dados.duracao}
+        onChangeText={(valor) => handleChange(valor, 'duracao')}
+      />
+
+      <TextInput
+        style={{ marginTop: 10 }}
+        mode='outlined'
+        label='Modalidade'
+        value={dados.modalidade}
+        onChangeText={(valor) => handleChange(valor, 'modalidade')}
+      />
+
+      <Button onPress={salvar}>Salvar</Button>
+
+    </ScrollView>
   )
 }
 
