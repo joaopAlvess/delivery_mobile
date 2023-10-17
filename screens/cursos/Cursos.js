80dba56226fcb4a025777996a2621bc14713e65a
Orion Teles de Mesquita <orionteles@gmail.com>
2023-10-16 21:46:43 -0300

    Tela de Curso

--- screens/cursos/Cursos.js
+++ screens/cursos/Cursos.js
@@ -1,36 +1,55 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useFocusEffect } from '@react-navigation/native'
 import React, { useEffect, useState } from 'react'
-import { Button, Text } from 'react-native-paper'
+import { ScrollView, View } from 'react-native'
+import { Button, Card, FAB, IconButton, MD3DarkTheme, Text } from 'react-native-paper'
 
 const Cursos = ({ navigation }) => {
 
   const [cursos, setCursos] = useState([])
 
-  useEffect(() => {
-    AsyncStorage.getItem('cursos').then(resultado => {
-      
-      resultado = JSON.parse(resultado) || []
+  useFocusEffect(
+    React.useCallback(() => {
 
-      console.log(resultado)
-      setCursos(resultado)
-    })
-  }, [])
+      AsyncStorage.getItem('cursos').then(resultado => {
+
+        resultado = JSON.parse(resultado) || []
+
+        console.log(resultado)
+        setCursos(resultado)
+      })
+
+    }, [])
+  );
 
   return (
     <>
-      <Text>Cursos</Text>
 
-      {cursos.map(item=>(
-        <Text>{item.nome}</Text>
-      ))}
-      
-      <Button
-        icon='plus'
-        mode='contained'
+      <ScrollView style={{ padding: 15 }}>
+
+        {cursos.map((item, i) => (
+          <Card key={i} mode='outlined' style={{ marginBottom: 10 }}>
+            <Card.Content>
+              <Text variant="titleLarge">{item.nome}</Text>
+              <Text variant="bodyMedium">Duração: {item.duracao} sem.</Text>
+              <Text variant="bodyMedium">Modalidade: {item.modalidade}</Text>
+            </Card.Content>
+            <Card.Actions>
+              <IconButton icon='pencil-outline' />
+              <IconButton icon='trash-can-outline' />
+            </Card.Actions>
+          </Card>
+        ))}
+
+
+      </ScrollView>
+
+      <FAB
+        icon="plus"
+        size='small'
+        style={{ position: 'absolute', right: 10, bottom: 10 }}
         onPress={() => navigation.push('cursos-form')}
-      >
-        Novo
-      </Button>
+      />
 
     </>
   )
