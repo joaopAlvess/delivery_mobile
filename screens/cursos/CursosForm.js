1b09532374b4ab381e47c45b8fcc8755af41fe6b
Orion Teles de Mesquita <orionteles@gmail.com>
2023-10-23 22:30:35 -0300

    Formik

--- screens/cursos/CursosForm.js
+++ screens/cursos/CursosForm.js
@@ -4,17 +4,12 @@ import { Formik } from 'formik'
 import React, { useState } from 'react'
 import { ScrollView, View } from 'react-native'
 import { Button, Text, TextInput } from 'react-native-paper'
+import * as Yup from 'yup';
 
 const CursosForm = ({ navigation, route }) => {
 
   const curso = route.params?.curso || {}
   const id = route.params?.id
-
-  // const [dados, setDados] = useState(curso)
-
-  // function handleChange(valor, campo) {
-  //   setDados({ ...dados, [campo]: valor })
-  // }
 
   function salvar(dados) {
 
@@ -35,15 +30,26 @@ const CursosForm = ({ navigation, route }) => {
 
   }
 
+  const cursoValidator = Yup.object().shape({
+    nome: Yup.string().strict()
+      .min(5, 'Valor muito curto')
+      .max(10, 'Valor muito grande')
+      .required('Campo obrigatório'),
+    duracao: Yup.number(),
+    modalidade: Yup.string().strict(),
+    teste: Yup.string().strict(),
+  },{ strict: true })
+
   return (
     <ScrollView style={{ margin: 15 }}>
       <Text>Formulário de Curso</Text>
 
       <Formik
         initialValues={curso}
+        validationSchema={cursoValidator}
         onSubmit={values => salvar(values)}
       >
-        {( {values, handleChange, handleSubmit} ) => (
+        {({ values, handleChange, handleSubmit, errors, touched }) => (
           <View>
             <TextInput
               style={{ marginTop: 10 }}
@@ -52,6 +58,11 @@ const CursosForm = ({ navigation, route }) => {
               value={values.nome}
               onChangeText={handleChange('nome')}
             />
+            {(errors.nome && touched.nome) &&
+              <Text style={{color: 'red', marginTop: 5}}>
+                {errors.nome}
+              </Text>
+            }
 
             <TextInput
               style={{ marginTop: 10 }}
@@ -61,6 +72,11 @@ const CursosForm = ({ navigation, route }) => {
               value={values.duracao}
               onChangeText={handleChange('duracao')}
             />
+            {(errors.duracao && touched.duracao) &&
+              <Text style={{color: 'red', marginTop: 5}}>
+                {errors.duracao}
+              </Text>
+            }
 
             <TextInput
               style={{ marginTop: 10 }}
@@ -69,6 +85,25 @@ const CursosForm = ({ navigation, route }) => {
               value={values.modalidade}
               onChangeText={handleChange('modalidade')}
             />
+            {(errors.modalidade && touched.modalidade) &&
+              <Text style={{color: 'red', marginTop: 5}}>
+                {errors.modalidade}
+              </Text>
+            }
+
+            <TextInput
+              style={{ marginTop: 10 }}
+              mode='outlined'
+              label='teste'
+              value={values.teste}
+              onChangeText={handleChange('teste')}
+            />
+            {(errors.teste && touched.teste) &&
+              <Text style={{color: 'red', marginTop: 5}}>
+                {errors.teste}
+              </Text>
+            }
+
             <Button onPress={handleSubmit}>Salvar</Button>
           </View>
         )}
